74cfdb014b399a7f4cfdee3ccf34bbf013a781cd
Ben Elsen <mail@benelsen.com>
2020-06-27 16:57:58 +0200

    Add LRUSet w/ tests

--- src/LRUSets.jl
+++ src/LRUSets.jl
@@ -1,5 +1,51 @@
+
 module LRUSets
 
-# Write your package code here.
+import Base: push!, length, values, eltype, iterate
+
+export LRUSet
+
+struct LRUSet{K}
+    max_size::Int
+    map::Dict{K, UInt64}
+    last::Threads.Atomic{UInt64}
+
+    function LRUSet{K}(max_size::Int, map::Dict{K, UInt64}) where {K}
+        last = length(map) > 0 ? maximum(values(map)) : UInt64(0)
+        new(max_size, map, Threads.Atomic{UInt64}(last))
+    end
+end
+
+function LRUSet{K}(max_size::Int) where {K}
+    LRUSet{K}(
+        max_size,
+        Dict{K, UInt64}(),
+    )
+end
+
+function LRUSet(max_size::Int, map::Dict{K, UInt64}) where {K}
+    LRUSet{K}(
+        max_size,
+        map,
+    )
+end
+
+LRUSet(max_size::Int) = LRUSet{Any}(max_size)
+
+length(ts::LRUSet) = length(ts.map)
+eltype(::Type{LRUSet{K}}) where {K} = K
+
+values(ts::LRUSet) = keys(ts.map)
+
+iterate(ts::LRUSet, state = 1) = iterate(keys(ts.map), state)
+
+function push!(ts::LRUSet{K}, e::K) where {K}
+    ts.map[e] = Threads.atomic_add!(ts.last, UInt64(1))
+    if length(ts) > ts.max_size
+        v, k = findmin(ts.map)
+        delete!(ts.map, k)
+    end
+    nothing
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,49 @@
-using LRUSets
+
 using Test
+using LRUSets
 
 @testset "LRUSets.jl" begin
-    # Write your tests here.
+
+    s = LRUSet(3)
+
+    @test s isa LRUSet{Any}
+    @test s.max_size === 3
+
+    @test length(s) === 0
+    @test issetequal(values(s), [])
+
+    push!(s, "foo")
+    @test length(s) === 1
+    @test issetequal(values(s), ["foo"])
+
+    push!(s, "foo")
+    @test length(s) === 1
+    @test issetequal(values(s), ["foo"])
+
+    push!(s, "bar")
+    push!(s, "baz")
+    push!(s, "faz")
+    @test length(s) === 3
+    @test issetequal(values(s), ["bar", "baz", "faz"])
+
+    s = LRUSet(
+        3,
+        Dict(
+            42 => UInt64(2),
+            10 => UInt64(3),
+        ),
+    )
+
+    @test s isa LRUSet{Int}
+    push!(s, 1)
+    @test issetequal(values(s), [1, 42, 10])
+
+    s = LRUSet{Integer}(2)
+    @test s isa LRUSet{Integer}
+
+    push!(s, Int16(21))
+    push!(s, Int128(42))
+    push!(s, UInt64(84))
+    push!(s, UInt8(255))
+    @test issetequal(values(s), [UInt64(84), UInt8(255)])
 end
