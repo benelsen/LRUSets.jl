04a33ed0145615cd852448dbdb7f2ceb5b18f9ef
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-06-27 15:21:41 +0000

    build based on 2820951

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#","page":"Home","title":"Home","text":"CurrentModule = LRUSets","category":"page"},{"location":"#LRUSets-1","page":"Home","title":"LRUSets","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [LRUSets]","category":"page"}]
+}
